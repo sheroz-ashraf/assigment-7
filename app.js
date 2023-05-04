ba367df0f527f04e77a3ca037d9be663a85f6bb2
shehroz <123664709+sheroz-ashraf@users.noreply.github.com>
2023-05-04 21:44:15 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,77 @@
+Chap 26-30
+
+//Question 1
+
+// var num=Number (prompt("Enter a Positive Integer"));
+// var round=Math.round(num);
+// var floor=Math.floor(num);
+// var ciel=Math.ceil(num);
+// document.write("number :"+ num )
+// document.write("<br>");
+// document.write("round off value :"+ round )
+// document.write("<br>");
+// document.write("floor value :"+ floor)
+// document.write("<br>");
+// document.write("ciel value :"+ ciel)
+
+
+//Question 2
+
+// var num=Number (prompt("Enter a Negative Integer"));
+// var round=Math.round(num);
+// var floor=Math.floor(num);
+// var ciel=Math.ceil(num);
+// document.write("number :"+ num )
+// document.write("<br>");
+// document.write("round off value :"+ round )
+// document.write("<br>");
+// document.write("floor value :"+ floor)
+// document.write("<br>");
+// document.write("ciel value :"+ ciel)
+
+
+//Question 3
+
+// var num=-4
+// var abs=Math.abs(num);
+// console.log(abs);
+
+//Question 4
+
+// var dice= Math.floor(Math.random()*6+1);
+// console.log(dice);
+
+//Question 5
+
+// var coin=Math.random();
+// if(coin<0.5){
+//     console.log("Heads")
+// }else{
+//     console.log("Tails")
+// }
+
+//Question 6
+
+// var num=Math.random()*100;
+// var round=Math.floor(Math.round(num)+1);
+// console.log(round);
+
+//Question 7
+
+// var wieght=prompt("Enter your wieght in kilograms");
+
+// wieght=parseFloat(wieght)
+
+// console.log(wieght);
+
+//Question 8
+
+// var secretNumber = Math.floor(Math.random() * 10) + 1;
+
+// var userNumber = prompt("Guess the secret number (between 1 and 10)");
+
+// if (userNumber === secretNumber) {
+//   console.log("Congratulations! You guessed the secret number!");
+// } else {
+//   console.log(`Sorry, the secret number was ${secretNumber}. Better luck next time!`);
+// }
